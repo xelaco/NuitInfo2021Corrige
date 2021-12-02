1495bb9caf7fe975fd3853a56eea41c068798563
xelaco <xelaco6@gmail.com>
2021-12-03 00:55:03 +0100

    Affichage des valeurs données dans le form.

--- form.php
+++ form.php
@@ -1,8 +1,12 @@
 <html>
 <body>
 
-Welcome <?php echo $_POST["name"]; ?><br>
-Your email address is: <?php echo $_POST["email"]; ?>
+Nom : <?php echo htmlspecialchars($_POST["nom"]); ?><br>
+Prénom : <?php echo htmlspecialchars($_POST["prenom"]); ?><br>
+Est sauveteur : <?php echo htmlspecialchars($_POST["estSauveteur"]); ?><br>
+Date : <?php echo htmlspecialchars($_POST["date"]); ?><br>
+Bateau : <?php echo htmlspecialchars($_POST["bateau"]); ?><br>
+Informations : <?php echo htmlspecialchars($_POST["informations"]); ?><br>
 
 </body>
 </html>

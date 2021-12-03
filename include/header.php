10b516cd0db0677a187171f9f04e49bd539af84c
ExyiN <jacquestea16@gmail.com>
2021-12-03 01:32:07 +0100

    Correction erreur

--- include/header.php
+++ include/header.php
@@ -1,11 +1,11 @@
 <div class="header-links">
-    <a class="link-home font-title" href="index.html" title="Accueil">Sauveteurs du dunkerquois</a>
+    <a class="link-home font-title" href="index.php" title="Accueil">Sauveteurs du dunkerquois</a>
     <nav class="menu-header">
         <ul class="menu-social">
             <li class="menu-link"><a href="https://www.facebook.com/groups/938396409644949"><i class="fab fa-facebook"></i></a></li>
             <li class="menu-link"><a href="https://twitter.com/@boutelierphili1"><i class="fab fa-twitter"></i></a></li>
             <li class="menu-link"><a href="mailto:sauveteurdudunkerquois@gmail.com"><i class="fas fa-envelope"></i></a></li>
-            <li class="menu-link"><a href="form.html"><i class="fas fa-toilet-paper"></i></a></li>
+            <li class="menu-link"><a href="form.php"><i class="fas fa-toilet-paper"></i></a></li>
         </ul>
     </nav>
 </div>

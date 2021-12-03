d7cd9469c0e366ba5f37c5c99b017de461ffe552
xelaco <xelaco6@gmail.com>
2021-12-03 02:08:14 +0100

    " --> '

--- formRecu.php
+++ formRecu.php
@@ -4,12 +4,12 @@
 <?php
 $nomFichier = "demandes/" . date("Y_m_d_H_i_s") . ".txt";
 $fichier = fopen($nomFichier, "w") or die("Erreur, veuillez recommencer s'il vous plait.");
-$texte = "Nom :" . htmlspecialchars($_POST["nom"]) . "\n"
-	. "Prénom :" . htmlspecialchars($_POST["prenom"]) . "\n"
-	. "Est sauveteur :" . htmlspecialchars($_POST["estSauveteur"]) . "\n"
-	. "Date :" . htmlspecialchars($_POST["date"]) . "\n"
-	. "Bateau :" . htmlspecialchars($_POST["bateau"]) . "\n"
-	. "Informations complémentaires :" . htmlspecialchars($_POST["informations"]) . "\n";
+$texte = 'Nom :' . htmlspecialchars($_POST["nom"]) . '\n'
+	. 'Prénom :' . htmlspecialchars($_POST["prenom"]) . '\n'
+	. 'Est sauveteur :' . htmlspecialchars($_POST["estSauveteur"]) . '\n'
+	. 'Date :' . htmlspecialchars($_POST["date"]) . '\n'
+	. 'Bateau :' . htmlspecialchars($_POST["bateau"]) . '\n'
+	. 'Informations complémentaires :' . htmlspecialchars($_POST["informations"]) . '\n';
 fwrite($fichier, $txt);
 fclose($fichier);
 ?>

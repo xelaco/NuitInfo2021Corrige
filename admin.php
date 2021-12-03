ca50ec587e54f20d7b8b48bff3e31a5e5b05cc93
xelaco <xelaco6@gmail.com>
2021-12-03 05:39:56 +0100

    Ajout boutons suppression fichier.

--- admin.php
+++ admin.php
@@ -1,4 +1,8 @@
 <?php
+function supprimer($f){
+	echo 'SUPPRIMER' . $f;
+}
+
 session_start();
 if(!isset($_SESSION["pseudo"])){
 	header("Location: connexion.php");
@@ -11,13 +15,21 @@ $fichiers = array_diff(scandir($dossier), array('.', '..', '.gitignore'));
 <html>
 	<body>
 		<p>Affichage des demandes d'ajout non traitées</p>
+<form action="" method="post" name="suppression">
 <?php
   foreach($fichiers as $f)
 	{
     $contenu = file_get_contents($dossier .'/' . $f);
 		echo '<pre>' . $contenu . '</pre>';
+
+		<input type="submit" name="supprimer" value="insert"/>
+		if($_POST['supprimer'] and $_SERVER['REQUEST_METHOD'] == "POST"){
+			supprimer($f);
+		}
+
 	}
 ?>
+</form>
 	<a href="deconnexion.php">Déconnexion</a>
 	</body>
 </html>

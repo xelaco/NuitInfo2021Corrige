342278fdf40885c7cd58bc79cceb0304b4ad1185
xelaco <xelaco6@gmail.com>
2021-12-03 04:20:26 +0100

    Retrait fonctions inutiles.

--- connexion.php
+++ connexion.php
@@ -5,10 +5,6 @@
 require('configBDD.php');
 session_start();
 if (isset($_POST['pseudo'])){
-	$pseudo = stripslashes($_REQUEST['pseudo']);
-	$pseudo = mysqli_real_escape_string($conn, $pseudo);
-	$mdp = stripslashes($_REQUEST['mdp']);
-	$mpd = mysqli_real_escape_string($conn, $mdp);
 	$requete = "SELECT * FROM `admins` WHERE pseudo='$pseudo' and mdp='".hash('sha256', $mdp)."'";
 	$resultat = mysqli_query($bdd, $requete) or die(mysql_error());
 	$occurrences = mysqli_num_rows($resultat);

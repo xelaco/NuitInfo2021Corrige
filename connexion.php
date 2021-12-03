98694d4ce83474334c8b945dc72ac3bee3f2e385
xelaco <xelaco6@gmail.com>
2021-12-03 03:40:30 +0100

    Ajout connexion.

---
+++ connexion.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<body>
+<?php
+require('config.php');
+session_start();
+if (isset($_POST['pseudo'])){
+	$pseudo = stripslashes($_REQUEST['pseudo']);
+	$pseudo = mysqli_real_escape_string($conn, $pseudo);
+	$mdp = stripslashes($_REQUEST['mdp']);
+	$mpd = mysqli_real_escape_string($conn, $mdp);
+	$query = "SELECT * FROM `admins` WHERE pseudo='$pseudo' and mdp='".hash('sha256', $mdp)."'";
+	$resultat = mysqli_query($conn, $query) or die(mysql_error());
+	$occurrences = mysqli_num_rows($resultat);
+	if($occurrences == 1){
+		$_SESSION['pseudo'] = $pseudo;
+		header("Location: admin.php");
+	}else{
+		$message = "Le nom d'utilisateur ou le mot de passe est incorrect.";
+	}
+}
+?>
+<form action="" method="post" name="login">
+<h1>Connexion</h1>
+<input type="text" name="username" placeholder="Nom d'utilisateur">
+<input type="password" name="mdp" placeholder="Mot de passe">
+<input type="submit" value="Connexion " name="submit">
+<?php if (! empty($message)) { ?>
+		<p class="errorMessage"><?php echo $message; ?></p>
+<?php } ?>
+</form>
+</body>
+</html>

263427a6c8de165830c86b269110560d22795c17
xelaco <xelaco6@gmail.com>
2021-12-03 07:04:32 +0100

    Correction erreur.

--- admin.php
+++ admin.php
@@ -16,14 +16,14 @@ $fichiers = array_diff(scandir($dossier), array('.', '..', '.gitignore'));
 	<body>
 		<p>Affichage des demandes d'ajout non traitées</p>
 <form action="" method="post" name="suppression">
-  <?php for(i = 0; i < fichiers.lenght(); i++): ?>
+  <?php for($i = 0; $i < fichiers.lenght(); $i++): ?>
 		<? $contenu = file_get_contents($dossier .'/' . $f[i]); ?>
 		<pre><?= $contenu; ?></pre>
 		<input type="submit" name="supprimer" value="insert"/>
     <?
       if($_POST['supprimer'] and $_SERVER['REQUEST_METHOD'] == "POST"){supprimer($f[i]);}
     ?>
-	<?php endfor; ?>
+	<?php endforeach; ?>
 </form>
 	<a href="deconnexion.php">Déconnexion</a>
 	</body>

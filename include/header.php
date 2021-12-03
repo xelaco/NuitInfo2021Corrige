a94c29eb3600f8d955b1957564f06cafd743d3ea
ExyiN <jacquestea16@gmail.com>
2021-12-03 03:21:50 +0100

    Plein de changements

--- include/header.php
+++ include/header.php
@@ -6,22 +6,8 @@
                 <li class="menu-link"><a href="https://www.facebook.com/groups/938396409644949"><i class="fab fa-facebook"></i></a></li>
                 <li class="menu-link"><a href="https://twitter.com/@boutelierphili1"><i class="fab fa-twitter"></i></a></li>
                 <li class="menu-link"><a href="mailto:sauveteurdudunkerquois@gmail.com"><i class="fas fa-envelope"></i></a></li>
-                <li class="menu-link"><a href="form.php"><i class="fas fa-toilet-paper"></i></a></li>
             </ul>
         </nav>
-    </div>
-    <div class="banner">
-        <label for="search" class="label-search font-default">270 ans d'histoire</label>
-    </div>
-    <div class="search-section">
-        <div class="search-box">
-            <input type="text" id="search" class="search-bar font-default" placeholder="Personne, bateau, mission...">
-            <button class="btn-search clickable"><i class="fas fa-search"></i></button>
-        </div>
-        <input type="checkbox" id="search-advanced" class="search-advanced-box display-none">
-        <label for="search-advanced" class="label-advanced font-default clickable">Recherche avancée <i class="fas fa-chevron-down"></i></label>
-        <form class="form-advanced">
-        </form>
     </div>
 </header>
 
--- index.php
+++ index.php
@@ -5,20 +5,22 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="css/global_style.css">
-    <link rel="stylesheet" type="text/css" href="css/index_style.css">
     <link rel="stylesheet" type="text/css" href="css/form_style.css">
+    <link rel="stylesheet" type="text/css" href="css/index_style.css">
     <link rel="icon" type="image/png" href="img/thomaspesquet.png">
     <script src="https://kit.fontawesome.com/798abe29a3.js" crossorigin="anonymous"></script>
     <title>Accueil - Sauveteurs du dunkerquois</title>
 </head>
 <body>
-    <?php include('include/header.php');?>
+    <?php include('include/header_index.php');?>
     <main>
-        
+        <section id="contribute" class="bg-dark">
+            <h2 class="font-title">Des infos qui manquent ?</h2>
+            <p class="font-default">Contribuez à la base et aidez-nous à enrichir le site !</p>
+            <a href="form.php" class="btn-link font-default">Formulaire d'ajout</a>
+        </section>
     </main>
-    <footer>
-
-    </footer>
+    <?php include('include/footer.php');?>
 </body>
 </html>
 

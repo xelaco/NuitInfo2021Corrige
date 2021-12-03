bda4d8b3aface4bbbc4fcb869c01c55e22a414d5
xelaco <xelaco6@gmail.com>
2021-12-03 02:13:05 +0100

    '\n' --> PHP_EOL

--- formRecu.php
+++ formRecu.php
@@ -4,12 +4,12 @@
 <?php
 $nomFichier = "demandes/" . date("Y_m_d_H_i_s") . ".txt";
 $fichier = fopen($nomFichier, "w") or die("Erreur, veuillez recommencer s'il vous plait.");
-$texte = 'Nom :' . htmlspecialchars($_POST["nom"]) . '\n'
-	. 'Prénom :' . htmlspecialchars($_POST["prenom"]) . '\n'
-	. 'Est sauveteur :' . htmlspecialchars($_POST["estSauveteur"]) . '\n'
-	. 'Date :' . htmlspecialchars($_POST["date"]) . '\n'
-	. 'Bateau :' . htmlspecialchars($_POST["bateau"]) . '\n'
-	. 'Informations complémentaires :' . htmlspecialchars($_POST["informations"]) . '\n';
+$texte = 'Nom :' . htmlspecialchars($_POST["nom"]) . PHP_EOL
+	. 'Prénom :' . htmlspecialchars($_POST["prenom"]) . PHP_EOL
+	. 'Est sauveteur :' . htmlspecialchars($_POST["estSauveteur"]) . PHP_EOL
+	. 'Date :' . htmlspecialchars($_POST["date"]) . PHP_EOL
+	. 'Bateau :' . htmlspecialchars($_POST["bateau"]) . PHP_EOL
+	. 'Informations complémentaires :' . htmlspecialchars($_POST["informations"]) . PHP_EOL;
 fwrite($fichier, $txt);
 fclose($fichier);
 ?>

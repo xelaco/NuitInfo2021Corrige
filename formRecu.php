199bdddd04724c61170823757b6ab93aa29eafa0
xelaco <xelaco6@gmail.com>
2021-12-03 01:22:34 +0100

    Changement nom fichier.

--- formRecu.php
+++ formRecu.php
@@ -8,6 +8,14 @@ Date : <?php echo htmlspecialchars($_POST["date"]); ?><br>
 Bateau : <?php echo htmlspecialchars($_POST["bateau"]); ?><br>
 Informations : <?php echo htmlspecialchars($_POST["informations"]); ?><br>
 
+<?php
+$nomFichier = date("Y_m_d_H_i_s") . "txt";
+$fichier = fopen($nomFichier, "w") or die("Erreur, veuillez recommencer s'il vous plait.");
+$txt = "Minnie Mouse\n";
+fwrite($fichier, $txt);
+fclose($fichier);
+?>
+
 </body>
 </html>
 

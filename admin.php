35a80056dd0414a7fa92144792d6170974a6d9d4
xelaco <xelaco6@gmail.com>
2021-12-03 05:46:50 +0100

    Correction erreur syntaxe.

--- admin.php
+++ admin.php
@@ -16,19 +16,16 @@ $fichiers = array_diff(scandir($dossier), array('.', '..', '.gitignore'));
 	<body>
 		<p>Affichage des demandes d'ajout non traitées</p>
 <form action="" method="post" name="suppression">
-<?php
-  foreach($fichiers as $f)
-	{
-    $contenu = file_get_contents($dossier .'/' . $f);
-		echo '<pre>' . $contenu . '</pre>';
-
-		<input type="submit" name="supprimer" value="insert"/>
-		if($_POST['supprimer'] and $_SERVER['REQUEST_METHOD'] == "POST"){
-			supprimer($f);
-		}
-
-	}
-?>
+<table>
+    <?php foreach($fichiers as $f): ?>
+    <tr>
+				<? $contenu = file_get_contents($dossier .'/' . $f); ?>
+				<td><?= $key; ?></td>
+		    <input type="submit" name="supprimer" value="insert"/>
+        <?if($_POST['supprimer'] and $_SERVER['REQUEST_METHOD'] == "POST"){supprimer($f);}?>
+    </tr>
+    <?php endforeach; ?>
+</table>
 </form>
 	<a href="deconnexion.php">Déconnexion</a>
 	</body>

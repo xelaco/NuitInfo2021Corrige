7e91720264cead677b902e5511fbffe09b666850
xelaco <xelaco6@gmail.com>
2021-12-03 02:28:34 +0100

    Debug

--- formRecu.php
+++ formRecu.php
@@ -1,18 +1,7 @@
 <html>
 <body>
 
-<?php
-$nomFichier = "demandes/" . date("Y_m_d_H_i_s") . ".txt";
-$fichier = fopen($nomFichier, "w") or die("Erreur, veuillez recommencer s'il vous plait.");
-$texte = 'Nom :' . $_POST["nom"] . PHP_EOL
-	. 'Prénom :' . htmlspecialchars($_POST["prenom"]) . PHP_EOL
-	. 'Est sauveteur :' . htmlspecialchars($_POST["estSauveteur"]) . PHP_EOL
-	. 'Date :' . htmlspecialchars($_POST["date"]) . PHP_EOL
-	. 'Bateau :' . htmlspecialchars($_POST["bateau"]) . PHP_EOL
-	. 'Informations complémentaires :' . htmlspecialchars($_POST["informations"]) . PHP_EOL;
-fwrite($fichier, $texte);
-fclose($fichier);
-?>
+<?php print_r($_POST); ?>
 
 Nom : <?php echo htmlspecialchars($_POST["nom"]); ?><br>
 Prénom : <?php echo htmlspecialchars($_POST["prenom"]); ?><br>
@@ -20,6 +9,19 @@ Est sauveteur : <?php echo htmlspecialchars($_POST["estSauveteur"]); ?><br>
 Date : <?php echo htmlspecialchars($_POST["date"]); ?><br>
 Bateau : <?php echo htmlspecialchars($_POST["bateau"]); ?><br>
 Informations : <?php echo htmlspecialchars($_POST["informations"]); ?><br>
+
+<?php
+$nomFichier = "demandes/" . date("Y_m_d_H_i_s") . ".txt";
+$texte = 'Nom :' . htmlspecialchars($_POST["nom"]) . PHP_EOL
+	. 'Prénom :' . htmlspecialchars($_POST["prenom"]) . PHP_EOL
+	. 'Est sauveteur :' . htmlspecialchars($_POST["estSauveteur"]) . PHP_EOL
+	. 'Date :' . htmlspecialchars($_POST["date"]) . PHP_EOL
+	. 'Bateau :' . htmlspecialchars($_POST["bateau"]) . PHP_EOL
+	. 'Informations complémentaires :' . htmlspecialchars($_POST["informations"]) . PHP_EOL;
+$fichier = fopen($nomFichier, "w") or die("Erreur, veuillez recommencer s'il vous plait.");
+fwrite($fichier, $texte);
+fclose($fichier);
+?>
 
 </body>
 </html>
